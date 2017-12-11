aaafb34dbbadce6d58dcddf7d4972cc907b39673
scruel <scruel@vip.qq.com>
2017-12-11 18:03:55 +0800

    Fix: chinese garbled. Due to read() method returns char rather than byte.

--- src/net/egork/chelper/tester/StringInputStream.java
+++ src/net/egork/chelper/tester/StringInputStream.java
@@ -2,22 +2,29 @@ package net.egork.chelper.tester;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author Egor Kulikov (kulikov@devexperts.com)
  */
 public class StringInputStream extends InputStream {
-    private final String s;
+    private final byte[] bytes;
     private int index = 0;
 
     public StringInputStream(String s) {
-        this.s = s;
+        byte[] bytes;
+        try {
+            bytes = s.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException ignore) {
+            bytes = s.getBytes();
+        }
+        this.bytes = bytes;
     }
 
     @Override
     public int read() throws IOException {
-        if (index < s.length())
-            return s.charAt(index++);
+        if (index < bytes.length)
+            return bytes[index++];
         return -1;
     }
 }
